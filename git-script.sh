fc026adb32e0e1297f7f990ab8227c6c18c0ce58
NewUsername1234 <ilja.romanov@yahoo.com>
2021-04-21 13:51:59 +0200

    hopefully last commit for now

--- git-script.sh
+++ git-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# "$*" takes in all the input after the script call and uses it as the commit message
+# "$*" takes in all the input after the script call
+# and uses it as the commit message
+# commit message doesn't need any quotes
 
 git add .
 git commit -m "$*"
